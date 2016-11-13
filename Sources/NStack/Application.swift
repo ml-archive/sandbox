fad9a1d0b8b7e752eb96349583e5aaed6163d147
Casper Rasmussen <cr@nodes.dk>
2016-11-13 22:05:33 +0100

    updated nstack init function

--- Sources/NStack/Application.swift
+++ Sources/NStack/Application.swift
@@ -1,16 +1,36 @@
 import Vapor
 
-struct Application{
+public struct Application{
     let name: String
     let applicationId: String
     let restKey: String
     let masterKey: String
     
     
-    public init(config: config) throws {
-        self.name = ""
-        self.applicationId = ""
-        self.restKey = ""
-        self.masterKey = ""
+    public init(config: Config) throws {
+        
+        // Set name
+        guard let name: String = config["name"]?.string else {
+            throw Abort.custom(status: .internalServerError, message: "NStack - Missing name in application init")
+        }
+        self.name = name;
+        
+        // Set applicationId
+        guard let applicationId: String = config["applicationId"]?.string else {
+            throw Abort.custom(status: .internalServerError, message: "NStack - Missing applicationId in application init")
+        }
+        self.applicationId = applicationId;
+        
+        // Set restKey
+        guard let restKey: String = config["restKey"]?.string else {
+            throw Abort.custom(status: .internalServerError, message: "NStack - Missing restKey in application init")
+        }
+        self.restKey = restKey;
+        
+        // Set masterKey
+        guard let masterKey: String = config["masterKey"]?.string else {
+            throw Abort.custom(status: .internalServerError, message: "NStack - Missing masterKey in application init")
+        }
+        self.masterKey = masterKey;
     }
 }
--- Sources/NStack/NStack.swift
+++ Sources/NStack/NStack.swift
@@ -4,18 +4,60 @@ import Foundation
 public final class NStack {
     
     let drop: Droplet
-    let config: Config
+    public let config: Config
+    public let applications: [Application]
+    public var application: Application
     
     public init(drop: Droplet) throws {
         self.drop = drop
         
+        // Set config
         guard let config: Config = drop.config["nstack"] else {
             throw Abort.custom(status: .internalServerError, message: "Missing nstack config")
         }
         
         self.config = config
         
+        // Set applications
+        var applications: [Application] = []
+        guard let applicationArr = self.config["applications"]?.array else {
+            throw Abort.serverError
+        }
+        
+        try applicationArr.forEach({
+            guard let config = $0 as? Config else {
+                throw Abort.serverError
+            }
+            
+            try applications.append(Application(config: config))
+        })
+        
+        
+        self.applications = applications
+    
+        // Set first application
+        guard let app: Application = applications.first else {
+            throw Abort.serverError
+        }
+        
+        self.application = app
+        
+        guard let defaultApplication: String = self.config["defaultApplication"]?.string else {
+            throw Abort.serverError
+        }
+        
+        try setApplication(name: defaultApplication)
     }
     
-    
+    public func setApplication(name: String) throws {
+        for application in applications {
+            if(application.name == name) {
+                self.application = application
+                
+                return
+            }
+        }
+        
+        throw Abort.custom(status: .internalServerError, message: "NStack - Application \(name) was not found")
+    }
 }
