de9ff1f33451fc0b084b5f3db10e448ca3073824
Casper Rasmussen <cr@nodes.dk>
2016-11-13 15:05:34 +0100

    decided for a direction

--- Sources/App/NMeta/Request+NMeta.swift
+++ Sources/App/NMeta/Request+NMeta.swift
@@ -1,8 +1,15 @@
 import HTTP
+import Vapor
 
 extension Request {
     var nMeta: NMeta? {
         get { return storage["nmeta"] as? NMeta }
         set { storage["nmeta"] = newValue }
+    }
+    
+    func isNMetaRequired() throws -> Bool {
+        
+        
+        return true
     }
 }
