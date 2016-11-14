56dc7d3ec46d95dd2ca08c156bd2e91f1581d4b9
Casper Rasmussen <cr@nodes.dk>
2016-11-14 21:35:02 +0100

    working on connection manager

---
+++ Sources/NStack/ConnectionMananger.swift
@@ -0,0 +1,12 @@
+import Foundation
+import Vapor
+
+final class ConnectionMananger {
+    let drop: Droplet
+
+    init(drop: Droplet) {
+        self.drop = drop
+    }
+    
+    func getTranslation
+}
