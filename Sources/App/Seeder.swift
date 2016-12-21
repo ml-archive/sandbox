68a0c21a9e8f9c2d8990184f7b4b3c32fb87d38c
Casper Rasmussen <cr@nodes.dk>
2016-12-21 22:47:08 +0100

    removed regular seeder

--- Sources/App/Seeder.swift
+++ Sources/App/Seeder.swift
@@ -19,55 +19,6 @@ public final class Seeder: Command {
         
         console.info("Started the seeder");
         
-        let backendUserRoles = [
-            try AdminPanel.BackendUserRole(node: [
-                    "title": "Super admin",
-                    "slug": "super-admin",
-                    "is_default": false,
-                ]),
-            try AdminPanel.BackendUserRole(node: [
-                "title": "Admin",
-                "slug": "admin",
-                "is_default": false,
-                ]),
-            try AdminPanel.BackendUserRole(node: [
-                "title": "User",
-                "slug": "user",
-                "is_default": true,
-                ]),
-            ]
-        
-        backendUserRoles.forEach({
-            var backendUserRole = $0
-            console.info("Looping \(backendUserRole.title)")
-            do {
-                try backendUserRole.save()
-            } catch {
-                console.error("Failed to store \(backendUserRole.title)")
-                print(error)
-            }
-        })
-        
-        
-        let backendUsers = [
-            try AdminPanel.BackendUser(node: [
-                "name": "Admin",
-                "email": "tech@nodes.dk",
-                "password": BCrypt.hash(password: "admin"),
-                "role": "super-admin",
-                ]),
-            ]
-        
-        backendUsers.forEach({
-            var backendUser = $0
-            console.info("Looping \(backendUser.name)")
-            do {
-                try backendUser.save()
-            } catch {
-                console.error("Failed to store \(backendUser.name)")
-                print(error)
-            }
-        })
         
         console.info("Finished the seeder");
     }
