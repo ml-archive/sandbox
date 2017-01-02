e579154e5bf2b4c83fe943237233523c6f20969d
Casper Rasmussen <cr@nodes.dk>
2017-01-02 17:55:37 +0100

    removed error

--- Sources/Error/Error.swift
+++
@@ -1,7 +0,0 @@
-import HTTP
-
-public enum Error: Swift.Error {
-    case standard(status: Status, message: String, code: Int)
-    case report(status: Status, message: String, code: Int)
-    case reportWithMeta(status: Status, message: String, code: Int, meta: [String: String])
-}
